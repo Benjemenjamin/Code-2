a3a43b415c021c79d2ce2d025212ac0f1b314875
Benjemenjamin <bennkiech@gmail.com>
2025-10-23 19:49:24 +0200

    Droid folder

---
+++ Droid/Droid.js
@@ -0,0 +1,9 @@
+"use strict";
+function getCommand(_state) {
+    return {
+        module: "Chassis",
+        method: "move",
+        data: "forward"
+    };
+}
+//# sourceMappingURL=Droid.js.map
