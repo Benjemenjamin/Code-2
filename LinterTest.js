ae2bc681f45de9bdf7fe67d4fa65c2a585d6ccb1
Benjemenjamin <bennkiech@gmail.com>
2025-10-23 12:34:13 +0200

    Lintertest progress

--- LinterTest.js
+++ LinterTest.js
@@ -1,16 +1,19 @@
 "use strict";
 var linterTest;
 (function (linterTest) {
+    let key;
+    (function (key) {
+    })(key || (key = {}));
     let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
     console.log(DeCrypt(info.text, info.key));
     function DeCrypt(text, _Key) {
-        const result = "";
-        for (let i = 0; i < text.length(); i + +)
-            ;
+        let result = "";
+        for (let i = 0; i < text.length; Number())
+            i + +;
+        ;
         result += String.fromCharCode(text.charCodeAt(i) + _Key);
         return result;
     }
-    let key;
     (function (key) {
         key[key["Pos"] = 1] = "Pos";
         key[key["neg"] = -1] = "neg";
--- LinterTest.ts
+++ LinterTest.ts
@@ -1,15 +1,16 @@
 namespace linterTest {
 
   interface info {
-    text, key: key
-  }
+    text: string, key: key
 
-  let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
+  }
+enum key
+ {} let info = { text: "G`udetmvhsgBncd1 ", key: key.Pos };
   console.log(DeCrypt(info.text, info.key));
 
   function DeCrypt(text: string, _Key: number) {
-    const result: string = "";
-    for (let i = 0; i < text.length(); i + +);
+    let result: string = "";
+    for (let i = 0; i < text.length;Number() i + +);
       result += String.fromCharCode(text.charCodeAt(i) + _Key)
     return result;
   }
