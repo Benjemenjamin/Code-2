179b75697c0f3eb06199256c860dec651060c1fc
Benjemenjamin <bennkiech@gmail.com>
2025-10-24 11:49:56 +0200

    Droid_Random

--- Droid/Droid.js
+++ Droid/Droid.js
@@ -1,11 +1,37 @@
 export var Droid;
 (function (Droid) {
+    // let counter: boolean = true
+    let random = 1;
     function getCommand() {
         const command = {
             module: "Chassis",
             method: "move",
-            data: "right"
+            data: "forward"
         };
+        random = Math.floor(Math.random() * 5);
+        switch (random) {
+            case 0:
+                command.data = "forward";
+                break;
+            case 1:
+                command.data = "back";
+                break;
+            case 2:
+                command.data = "left";
+                break;
+            case 3:
+                command.data = "right";
+                break;
+            case 4:
+                command.data = "stop";
+                break;
+        }
+        // if (counter == false) {
+        //   command.data = "left"
+        //   counter = true;
+        //} else {
+        //     counter = false;
+        // }
         console.log(command);
         return command;
     }
--- Droid/Droid.ts
+++ Droid/Droid.ts
@@ -1,21 +1,55 @@
 export namespace Droid {
 
-interface Command {
-    module: string
-    method: string
-    data: string
-}
+    // let counter: boolean = true
+    let random: number = 1
 
-
-export function getCommand(): Command{
-    const command: Command = {
-
-        module: "Chassis",
-        method: "move",
-        data: "right"
+    interface Command {
+        module: string
+        method: string
+        data: string
     }
-    console.log(command)
-    return command;
-}
+
+
+
+
+
+
+    export function getCommand(): Command {
+        const command: Command = {
+
+            module: "Chassis",
+            method: "move",
+            data: "forward"
+        }
+        random = Math.floor(Math.random() * 5)
+        switch (random) {
+            case 0:
+                command.data = "forward"
+                break;
+            case 1:
+                command.data = "back"
+                break;
+            case 2:
+                command.data = "left"
+                break;
+            case 3:
+                command.data = "right"
+                break;
+            case 4:
+                command.data = "stop"
+                break;
+        }
+
+        // if (counter == false) {
+        //   command.data = "left"
+        //   counter = true;
+        //} else {
+        //     counter = false;
+        // }
+
+
+        console.log(command)
+        return command;
+    }
 
 }
